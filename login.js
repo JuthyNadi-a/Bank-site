fbe8dfbe0a29503ddfac1f833c4f285972ca40c1
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-03-29 23:41:20 +0300

    getting input of email and password

---
+++ login.js
@@ -0,0 +1,8 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+})
