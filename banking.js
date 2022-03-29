e9f2a509a350a66212a10eedef996af1fcbeb5ea
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-03-29 23:49:26 +0300

    getting deposit amount and updating deposit total

---
+++ banking.js
@@ -0,0 +1,15 @@
+// handle deposit button event
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+    // update deposit total
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+});
