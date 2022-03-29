fd79edd841689451cbc29579c57feb9b0ff23c89
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-03-30 00:22:54 +0300

    getting withdrawing amount and updating withdrawal total

--- banking.js
+++ banking.js
@@ -20,3 +20,19 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // clear the deposit input field
     depositInput.value = '';
 });
+
+// handle withdraw event handler
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // getting withdrawal amout
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+    // set withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+});
